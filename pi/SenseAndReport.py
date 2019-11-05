a2a10f532406e445a41837f2f1e753ea632437fc
gidoBOSSftw5731 <gido5731@gido-desk-deb.imagen.click>
2019-11-04 22:52:31 -0500

    back to 40 and add pull down

--- pi/SenseAndReport.py
+++ pi/SenseAndReport.py
@@ -9,13 +9,13 @@ import time
 import RPi.GPIO as GPIO
 
 ## Set basic variables: GPIO pin and file archive location.
-switch1 = 38
+switch1 = 40
 #file_location = '/var/tmp/pipicts'
 
 ## Use board pin numbering.
 GPIO.setmode(GPIO.BOARD)
 ## Setup GPIO Pin 40 to IN
-GPIO.setup(switch1, GPIO.IN)
+GPIO.setup(switch1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 ## Turn on GPIO pin 40
 GPIO.input(switch1)
 print('ACTIVATED')
